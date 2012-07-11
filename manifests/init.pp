2c3d1f622ac2885282b36ab2765c8bd81a8df34d
James Fryman <james@frymanet.com>
2012-07-11 17:01:54 -0500

    fixing init.pp to be a little more readable

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,38 +35,28 @@
 #    commonName          => 'irc.frymanandassociates.net',
 #  }
 class znc(
-  $auth_type           = '',
-  $ssl                 = '',
-  $organizationName    = '',
-  $localityName        = '',
-  $stateOrProvinceName = '',
-  $countryName         = '',
-  $emailAddress        = '',
-  $commonName          = '',
-  $port                = ''
-) {
+  $auth_type           = $znc::params::zc_auth_type,
+  $ssl                 = $znc::params::zc_ssl,
+  $organizationName    = undef,
+  $localityName        = undef,
+  $stateOrProvinceName = undef,
+  $countryName         = undef,
+  $emailAddress        = undef,
+  $commonName          = undef,
+  $port                = $znc::params::zc_port
+) inherits znc::params {
   include stdlib
-  include znc::params
+
   if $auth_type == 'sasl' { include saslauthd }
-  
-  ### Begin Parameter Setting ###
-  if $auth_type == '' { $REAL_auth_type = $znc::params::zc_auth_type }
-  else { $REAL_auth_type = $auth_type }
-  
-  if $ssl == '' { $REAL_ssl = $znc::params::zc_ssl }
-  else { $REAL_ssl = $ssl }
-  
-  if $port == '' { $REAL_port = $znc::params::zc_port }
-  else { $REAL_port = $port }
 
   # Make sure that all of the SSL parameters are filled out.
   if ($ssl == 'true') and 
-    ($organizationName == '' or
-     $localityName == '' or 
-     $stateOrProvinceName == '' or 
-     $countryName == '' or 
-     $emailAddress == '' or 
-     $commonName == '') 
+    ($organizationName == undef or
+     $localityName == undef or 
+     $stateOrProvinceName == undef or 
+     $countryName == undef or 
+     $emailAddress == undef or 
+     $commonName == undef) 
   {
     fail("Missing Parameters to generate an SSL Certificate")   
   }
@@ -75,15 +65,15 @@ class znc(
   anchor { 'znc::begin': }
   -> class { 'znc::package': }
   -> class { 'znc::config': 
-       auth_type           => $REAL_auth_type,
-       ssl                 => $REAL_ssl,
+       auth_type           => $auth_type,
+       ssl                 => $ssl,
        organizationName    => $organizationName,
        localityName        => $localityName,
        stateOrProvinceName => $stateOrProvinceName,
        countryName         => $countryName,
        emailAddress        => $emailAddress,
        commonName          => $commonName,
-       port                => $REAL_port,
+       port                => $port,
      }
   ~> class { 'znc::service': }
   -> anchor { 'znc::end': }
